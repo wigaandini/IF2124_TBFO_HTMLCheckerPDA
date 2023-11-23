b55f649def3e459a3bc296a8e40aa6dc8b835458
denoseu <denisefeliciatiowanni@gmail.com>
2023-11-23 13:19:50 +0700

    fix: main.py
    still have to add/ handle some states in pda

--- src/main.py
+++ src/main.py
@@ -26,82 +26,49 @@ def tokenize_html_with_regex(html_content):
             # misal id= 'sesuatu', sesuatunya itu gaperlu diperhatiin jadi yang di keep cuman id= ''
             attributes = re.sub(r'(["\'])(?:(?=(\\?))\2.)*?\1', '', attributes)
         if closing_tag:
-            closing_tag = '/' + closing_tag
-        filtered_matches.append((opening_tag, attributes, closing_tag))
+            # Modify here to include the closing tag in the format '/closing_tag'
+            filtered_matches.append('/' + closing_tag)
+        # Append the opening tag, but only if it is not an empty string
+        if opening_tag:
+            filtered_matches.append(opening_tag)
 
     return filtered_matches
+
 
 ################## MAIN ##################
 if __name__ == "__main__":
     # baca file pda
-    file_path = "src/pda.txt"
+    file_path = "pda.txt"
     pda_matrix = read_txt_to_matrix(file_path)
     # print(pda_matrix[0])
 
     # baca file html
-    file_path = 'src/text.html'
+    file_path = 'text.html'
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
 
     # tokenize html
     html_tags = tokenize_html_with_regex(html_content)
+    print(html_tags)
 
-    opening_tags = [opening_tag for opening_tag, _, _ in html_tags if opening_tag]
-    closing_tags = [closing_tag for _, _, closing_tag in html_tags if closing_tag]
-    # print(len(opening_tags))
-    # print(opening_tags)
-    # print(closing_tags)
-
-    # Exclude tags that don't require closing
-    tags_without_closing = {"br", "img", "hr", "input", "link", "!DOCTYPE", "!--"}
-    opening_tags = [tag for tag in opening_tags if tag not in tags_without_closing]
-    closing_tags = [tag for tag in closing_tags if tag not in tags_without_closing]
-    print(opening_tags)
-    print(closing_tags)
-    # print(tags_without_closing)
-        
-    # print tags and attributes
-    # for opening_tag, attributes, closing_tag in html_tags:
-    #     if opening_tag:
-    #         print(f'Tag: {opening_tag}, Attributes: {attributes}')
-    #     else:
-    #         print(f'Tag: {closing_tag}, Attributes:')
-
-    # udah pasti ga sama
-    if (len(opening_tags) != len(closing_tags)): # blm tentu krn ada bbrp opening tag yg gaperlu closing tag
-        print("Syntax Error")
-    else:
-        state = pda_matrix[0][0]
-        for opening_tag in opening_tags:
-            found_opening = False
-            for row in pda_matrix:
-                if opening_tag == row[1]:
-                    found_opening = True
-                    state = row[3]
-                    break
-
-            if not found_opening:
-                print(f"Error: Opening tag '{opening_tag}' is not valid.")
+    state = pda_matrix[0][0]
+    for tag in html_tags:
+        found = False
+        for row in pda_matrix:
+            if tag == row[1] and state == row[0]:
+                found_opening = True
+                state = row[3]
                 break
-        
-        if found_opening:
-            for closing_tag in closing_tags:
-                found_closing = False
-                for row in pda_matrix:
-                    if closing_tag == row[1]:
-                        found_closing = True
-                        state = row[3]
-                        break
 
-                if not found_closing:
-                    print(f"Error: Closing tag '{closing_tag}' is not valid.")
-                    break
+        if not found_opening:
+            print(f"Error: Tag '{tag}' is not valid.")
+            break
 
-        print("Final state:", state)
+    print("Final state:", state)
 
-        if (state == "FINAL"):
-            print("Accepted.")
-        else:
-            print("Syntax Error.")
+    if (state == "FINAL"):
+        print("Accepted.")
+    else:
+        print("Syntax Error.")
 
         

a5632cd9d573665cdfc65a4f32e4e50d55434079
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2023-11-26 23:28:09 +0700

    final amin

--- src/main.py
+++ src/main.py
@@ -27,6 +27,10 @@ def tokenize_html_with_regex(html_content):
 
     matches = tag_pattern.findall(html_content)
     filtered_matches = []
+    method_values = []
+    input_types = []
+    button_types = []
+
     for opening_tag, attributes, closing_tag, comment_content in matches:
         if opening_tag:
             filtered_matches.append(opening_tag)
@@ -44,13 +48,57 @@ def tokenize_html_with_regex(html_content):
                 elif unquoted:
                     filtered_matches.append(unquoted)
 
+                # Check for method attribute
+                if opening_tag.lower() == 'form' and attr_name.lower() == 'method=':
+                    method_values.append(double_quoted or single_quoted or unquoted)
+
+                # Check for input type attribute
+                if opening_tag.lower() == 'input' and attr_name.lower() == 'type=':
+                    input_types.append(double_quoted or single_quoted or unquoted)
+
+                # Check for input type attribute
+                if opening_tag.lower() == 'button' and attr_name.lower() == 'type=':
+                    button_types.append(double_quoted or single_quoted or unquoted)
+
         if closing_tag:
             filtered_matches.append('/' + closing_tag)
 
         if comment_content:
             continue
 
-    return filtered_matches
+    return filtered_matches, method_values, input_types, button_types
+
+################## ATTRIBUTES FOR FORM-METHOD, INPUT TYPES, BUTTON TYPES ##################
+def add_method_values(html_arr, method_values):
+    i = 0
+    j = 0
+    while i < len(html_arr):
+        if html_arr[i] == 'method=' and html_arr[i + 1] == '"':
+            html_arr.insert(i + 2, method_values[j])
+            i += 3
+            j += 1
+        else:
+            i += 1
+
+def add_input_types(html_arr, input_types):
+    i = 0
+    j = 0
+    while i < len(html_arr):
+        if html_arr[i] == 'input' and html_arr[i + 1] == 'type=' and html_arr[i + 2] == '"':
+            html_arr.insert(i + 3, input_types[j])
+            i += 4
+            j += 1
+        i += 1
+
+def add_button_types(html_arr, button_types):
+    i = 0
+    j = 0
+    while i < len(html_arr):
+        if html_arr[i] == 'button' and html_arr[i + 1] == 'type=' and html_arr[i + 2] == '"':
+            html_arr.insert(i + 3, button_types[j])
+            i += 4
+            j += 1
+        i += 1
 
 ################## MAIN ##################
 if __name__ == "__main__":
@@ -74,9 +122,12 @@ if __name__ == "__main__":
     pda_file_path = "src/pda.txt"
     pda_matrix = read_txt_to_matrix(pda_file_path)
 
+    # baca file html
+    print("")
     # cek filenya exist/tidak
     html_file_name = input("Enter the file name for HTML (.html): ")
     html_file_path = get_HTML_path(html_file_name)
+    
     while not Path(html_file_path).exists():
         print(f"\nError: File '{html_file_name}' does not exist in 'test' folder.")
         html_file_name = input("Enter the file name for HTML (.html): ")
@@ -86,7 +137,11 @@ if __name__ == "__main__":
         html_content = file.read()
 
     # tokenize html
-    html_tags = tokenize_html_with_regex(html_content)
+    filename = pda_file_path.split('/')[-1]
+    html_tags, method_values, input_types, button_types = tokenize_html_with_regex(html_content)
+    add_method_values(html_tags, method_values)
+    add_input_types(html_tags, input_types)
+    add_button_types(html_tags, button_types)
     print("")
     print("HTML tags found in " + html_file_name + ":")
     print(html_tags)
@@ -97,14 +152,17 @@ if __name__ == "__main__":
         found = False
         for row in pda_matrix:
             if tag == row[1] and state == row[0]:
+                print("Processing " + tag + "...")
                 found = True
                 state = row[3]
+                print("Current state:", state)
                 break
-
+            
         if not found:
-            print(f"Error: Tag '{tag}' is not valid because the previous tag isn't complete.")
+            # print(f"Error: Tag '{tag}' is not valid because the previous tag isn't complete.")
             break
 
+    print("")
     print("Final state:", state)
 
     if state == "FINAL":
